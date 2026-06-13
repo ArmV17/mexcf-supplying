49a2287b974e53883a551b0cf04e3eed571fc1d0
ArmV17 <22040111@alumno.utc.edu.mx>
2026-06-13 17:36:19 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mexcf-supplying',
+  webDir: 'www'
+};
+
+export default config;
