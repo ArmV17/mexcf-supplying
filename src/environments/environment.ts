c1722a3c9f9724ffa964eab1bf800f0452bab16c
ArmV17 <22040111@alumno.utc.edu.mx>
2026-06-15 13:51:35 -0600

    App V1.1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyB3jN0Dpyd6AdVzrbzngbBYvrHbQJMYHXI",
+    authDomain: "supplying-73778.firebaseapp.com",
+    projectId: "supplying-73778",
+    storageBucket: "supplying-73778.firebasestorage.app",
+    messagingSenderId: "260802938878",
+    appId: "1:260802938878:web:098b28757fc08a0dede2af",
+    measurementId: "G-QBHSTVXE45"
   }
 };
